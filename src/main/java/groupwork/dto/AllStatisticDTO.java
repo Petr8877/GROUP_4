f55c469b44b984a784e3c4d0f3b2f435f18ad870
Yuliya Romashko <ramashkay@gmail.com>
2023-01-14 17:09:29 +0300

    toString AllStatisticDTO is changed

--- src/main/java/groupwork/dto/AllStatisticDTO.java
+++ src/main/java/groupwork/dto/AllStatisticDTO.java
@@ -30,29 +30,5 @@ public class AllStatisticDTO {
         return mapUserInfo;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss dd-MM-yyyy");
 
-        builder.append("Результаты голосования за лучшего исполнителя: " + "\n");
-
-        for (Map.Entry<SingerDTO, Integer> entry : mapSingers.entrySet()) {
-            builder.append(entry.getKey()).append(" -> ").append(entry.getValue()).append("\n");
-        }
-
-        builder.append("Результаты голосования за лучший жанр: " + "\n");
-
-        for (Map.Entry<GenreDTO, Integer> entry : mapGenres.entrySet()) {
-            builder.append(entry.getKey()).append(" -> ").append(entry.getValue()).append("\n");
-        }
-
-        builder.append("О пользователе: " + "\n");
-
-        for (Map.Entry<String, LocalDateTime> entry : mapUserInfo.entrySet()) {
-            builder.append(entry.getKey()).append(" -> ").append(dtf.format(entry.getValue())).append("\n");
-        }
-
-        return builder.toString();
-    }
 }
