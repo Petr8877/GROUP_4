59983b1ae1ae87f8878511858098645dca929620
Aleksey Apanas <lexaapanas@mail.ru>
2023-01-19 23:01:22 +0300

    change ddl

--- posgres/ddl/04_votes.sql
+++ posgres/ddl/04_votes.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS app.votes
     id bigserial,
     dt_create timestamp without time zone,
     about text NOT NULL,
+    mail text NOT NULL,
     CONSTRAINT votes_pkey PRIMARY KEY (id)
 )
