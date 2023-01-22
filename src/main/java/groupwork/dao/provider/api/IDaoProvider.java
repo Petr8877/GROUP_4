6a900f86dfd1cd85957174138c5c2aa3be17e45d
Aleksey Apanas <lexaapanas@mail.ru>
2023-01-22 23:01:41 +0300

    update provider

---
+++ src/main/java/groupwork/dao/provider/api/IDaoProvider.java
@@ -0,0 +1,11 @@
+package groupwork.dao.provider.api;
+
+import groupwork.dao.api.IGenreDao;
+import groupwork.dao.api.ISingerDao;
+import groupwork.dao.api.IVotingDao;
+
+public interface IDaoProvider {
+    IGenreDao genreDao();
+    ISingerDao singerDao();
+    IVotingDao voteDao();
+}
